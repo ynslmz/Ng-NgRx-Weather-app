875e8939a3f7d61574a64ca0314e62fbd7af5df2
Yunus ÖLMEZ <yns4832@gmail.com>
2021-09-13 23:15:28 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SimpleWeatherApp';
+}
